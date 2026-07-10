dafb128f438d3f4a6749546e45866da8b7320608
s-jagdale <jagdalesanika1234@gmail.com>
2026-07-11 00:49:29 +0530

    fix CORS origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://rag-recruiter.vercel.app/",
+        "https://rag-recruiter.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
