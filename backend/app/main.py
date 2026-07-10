8fcc5d55386489e13d4d5a269c0449b46441b350
s-jagdale <jagdalesanika1234@gmail.com>
2026-07-11 00:40:04 +0530

    Update CORS for Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://pxvxzzfj-5173.inc1.devtunnels.ms",
+        "https://rag-recruiter.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
